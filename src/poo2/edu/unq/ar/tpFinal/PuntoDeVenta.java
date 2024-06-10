727c55a47f9bcc2b92cdc8b69d09e276f609c5e9
Gonzalo Uribarri <gonzalouribarri@hotmail.com>
2024-06-09 21:09:25 -0300

    Tests de punto de venta y modelo del mismo

--- src/poo2/edu/unq/ar/tpFinal/PuntoDeVenta.java
+++ src/poo2/edu/unq/ar/tpFinal/PuntoDeVenta.java
@@ -1,5 +1,32 @@
 package poo2.edu.unq.ar.tpFinal;
 
-public class PuntoDeVenta {
+import java.time.LocalTime;
 
+public class PuntoDeVenta {
+	
+	private SEM sistema;
+	private ZonaDeEstacionamiento zona;
+	
+	public PuntoDeVenta(SEM sistema, ZonaDeEstacionamiento zona) {
+		this.sistema = sistema;
+		this.zona = zona;
+	}
+
+	public void cargarCredito(Double montoACargar, String celular) {
+		this.sistema.cargarCredito(montoACargar, celular);
+	}
+
+	public EstacionamientoCompraPuntual registarEstacionamiento(String patente, int cantidadDeHoras) throws Exception {
+		EstacionamientoCompraPuntual estacionamientoARegistrar = new EstacionamientoCompraPuntual(LocalTime.now(), patente, cantidadDeHoras);
+		this.getSem().registrarUnNuevoEstacionamientoEnLaZona(estacionamientoARegistrar, this.getZona());
+		return estacionamientoARegistrar;
+	}
+
+	private ZonaDeEstacionamiento getZona() {
+		return this.zona;
+	}
+
+	private SEM getSem() {
+		return this.sistema;
+	}
 }
