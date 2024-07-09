908b08df3e3ee0100794e89bc1d99ff75ca3f73f
Fabricio <fachucamilla32@gmail.com>
2024-07-09 18:29:48 -0300

    tets estacionamiento via app

--- test/poo2/edu/unq/ar/tpFinal/TestEstacionamientoViaApp.java
+++ test/poo2/edu/unq/ar/tpFinal/TestEstacionamientoViaApp.java
@@ -6,6 +6,8 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -23,12 +25,13 @@ public class TestEstacionamientoViaApp {
 		inspector = mock(AppInspector.class);
 		sem = mock(SEM.class);
 		appUsuario = mock(AppDeUsuario.class);
-		estacionamiento = new EstacionamientoViaApp(appUsuario, LocalTime.of(7, 0), LocalTime.of(20, 0), "AA20AA");
+		estacionamiento = new EstacionamientoViaApp(appUsuario, LocalDateTime.of(LocalDate.now(), LocalTime.of(7, 0)),
+				LocalDateTime.of(LocalDate.now(), LocalTime.of(20, 0)), "AA20AA");
 	}
 
 	@Test
 	void unEstacionamientoCuandoEsCreadoTieneUnaHoraDeInicio() {
-		assertEquals(estacionamiento.getHoraInicio(), LocalTime.of(7, 0));
+		assertEquals(estacionamiento.getHoraInicio(), LocalDateTime.of(LocalDate.now(), LocalTime.of(7, 0)));
 	}
 
 	@Test
